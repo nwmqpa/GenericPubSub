f4a2d35fcb73d17d05e041afe998b594c0082a83
Thomas Nicollet <thomas.nicollet@epitech.eu>
2020-05-13 17:01:48 +0200

    :bug: Change `dynamic` to `object` to avoind binding a DLL

--- GenericPubSub/PubSub/PubSub.cs
+++ GenericPubSub/PubSub/PubSub.cs
@@ -9,7 +9,7 @@ namespace GenericPubSub.PubSub
         
         
         private static Dictionary<Type, List<Action<object>>> listeners = new Dictionary<Type, List<Action<object>>>();
-        private static Dictionary<string, List<Action<Dictionary<string, dynamic>>>> namedListeners = new Dictionary<string, List<Action<Dictionary<string, dynamic>>>>();
+        private static Dictionary<string, List<Action<Dictionary<string, object>>>> namedListeners = new Dictionary<string, List<Action<Dictionary<string, object>>>>();
 
         public static void RegisterListener<T>(Action<object> listener) where T : class
         {
@@ -18,10 +18,10 @@ namespace GenericPubSub.PubSub
             listeners[typeof(T)].Add(listener);
         }
         
-        public static void RegisterListener(Action<Dictionary<string, dynamic>> listener, string eventTypeName)
+        public static void RegisterListener(Action<Dictionary<string, object>> listener, string eventTypeName)
         {
             if (namedListeners.ContainsKey(eventTypeName) == false)
-                namedListeners.Add(eventTypeName, new List<Action<Dictionary<string, dynamic>>>());
+                namedListeners.Add(eventTypeName, new List<Action<Dictionary<string, object>>>());
             namedListeners[eventTypeName].Add(listener);
         }
 
